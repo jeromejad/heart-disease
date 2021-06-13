380a1755963c74f843b657f461b84e8b51929620
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-06-13 21:37:42 +0530

    svse

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var router = express.Router();
+var connect = require("connect");
+
+var app = connect.createServer().use(connect.static(__dirname));
 
 
 router.get("/", function(req, res){
-	res.render("index.html");
+	res.render("/index.html");
 	
 });
 
